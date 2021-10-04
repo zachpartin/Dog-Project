751399c018c60f5de25bf7d5d19a70c6fda279fe
Zach Partin <zachpartin@zachs-mbp.myfiosgateway.com>
2021-10-04 17:44:21 -0400

    got function working

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ const BASE_URL = "https://api.thedogapi.com/v1/breeds"
 const API_KEY = "925c2150-f558-4883-9b84-a3973d3ed76c"
 const API_URL = `${BASE_URL}?${API_KEY}`
 const breedSearchURL = "https://api.thedogapi.com/v1/images/search"
-const breedSearch = `${breedSearchUrl}?${API_KEY}`
+const breedSearch = `${breedSearchURL}?${API_KEY}&breed_id=`
 //To find specific breed add 'breed_id=(id)' as query parameter
 
 console.log(API_URL);
@@ -27,17 +27,18 @@ fetch(API_URL)
     console.log(error);
 })
 
-// const pullDogBreed = (breedId) => {
-//   fetch(`${API_URL})
-//   .then((response) => {
-//     return response.json();
-//   })
-//   .then((responseJson) => {
-//     console.log(responseJson);
-//   })
-//   .catch((error) => {
-//     console.log(error);
-// })
-// }
+const pullDogBreed = (breedId) => {
+  fetch(`${breedSearch}`+ breedId)
+  .then((response) => {
+    return response.json();
+  })
+  .then((responseJson) => {
+    console.log(responseJson);
+  })
+  .catch((error) => {
+    console.log(error);
+})
+}
 
+pullDogBreed(5)
 
