d228323e034986e17fd169f8821fc113bbdbed72
Zach Partin <zachpartin@zachs-mbp.myfiosgateway.com>
2021-10-04 10:28:02 -0400

    JS file is connected and logging to DOM

---
+++ script.js
@@ -0,0 +1,13 @@
+/*
+1. Connect API
+2. Add event listener for when size is selected from dropdown list
+3. Get selection to pull info from Dog API for a random dog that matches the size selected
+4. 
+*/
+
+console.log("Connected");
+
+const newP = document.createElement('p')
+document.body.appendChild(newP);
+
+
